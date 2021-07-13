988e6ec08c5742555278a5add0c046c8f240f9e6
DalatCoder <hieuntctk42@gmail.com>
2021-07-13 10:58:42 +0700

    Plugin to log user when they logging to WPAdmin

---
+++ 01.basic/wordpress/wp-content/plugins/03.notice-admin/notice-admin.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Plugin Name: Notice Admin
+ * Description: Notice Admin
+ * Version: 1.0
+ * Author: 2nth
+ */
+
+require __DIR__ . '/functions.php';
+
+class NoticeAdmin
+{
+    function __construct()
+    {
+        add_action('admin_notices', [$this, 'showNotice']);
+
+        add_action('wp_login', [$this, 'createUserLogEntry'], 10, 2);
+    }
+
+    function showNotice()
+    {
+        $user = wp_get_current_user();
+        echo 'Hello ' . $user->display_name . '<br>';
+    }
+
+    function createUserLogEntry($user_login_name, $userObject)
+    {
+        $args = [
+            'post_type' => 'post',
+            'post_title' => 'Login by ' . $user_login_name . ' at ' . date("H:i:s m/d/Y"),
+            'post_status' => 'private',
+            # 'post_category' => []
+            # 'post_author' => $userObject->ID
+        ];
+
+        $postId = wp_insert_post($args);
+
+        # LogUtils::writeLog('Create log with post ID: ' . $postId);
+    }
+}
+
+$noticeAdmin = new NoticeAdmin();
