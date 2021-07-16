6d1f97e7e87b0d9e4ce59640ce5a0581bc05886b
DalatCoder <hieuntctk42@gmail.com>
2021-07-16 10:09:53 +0700

    Display tab

--- 01.basic/wordpress/wp-content/plugins/12.tab-instruction/index.php
+++ 01.basic/wordpress/wp-content/plugins/12.tab-instruction/index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /*
@@ -50,17 +49,76 @@ class NTHTabInstruction
 
     function renderMenuPage()
     {
+        // Hide all sections by default
         $show_tab_page1 = 'none';
         $show_tab_page2 = 'none';
         $show_tab_page3 = 'none';
         $show_tab_page4 = 'none';
 
+        // Set the active tab
         $tab_active_page1 = '';
         $tab_active_page2 = '';
         $tab_active_page3 = '';
         $tab_active_page4 = '';
 
-        echo "HTML ";
+        if ($_GET['tab']) {
+            switch ($_GET['tab']) {
+                case '2':
+                    $show_tab_page2 = 'block';
+                    $tab_active_page2 = 'tab-active';
+                    break;
+                case '3':
+                    $show_tab_page3 = 'block';
+                    $tab_active_page3 = 'tab-active';
+                    break;
+                case '4':
+                    $show_tab_page4 = 'block';
+                    $tab_active_page4 = 'tab-active';
+                    break;
+                default:
+                    $show_tab_page1 = 'block';
+                    $tab_active_page1 = 'tab-active';
+                    break;
+            }
+        } else {
+            // Tab 1 is set by default
+            $show_tab_page1 = 'block';
+            $tab_active_page1 = 'tab-active';
+        }
+
+        echo '
+            <h2 class="nav-tab-wrapper">
+                <a href="' . get_admin_url() . 'admin.php?page=nth-instructions&tab=1" id="tab-page1" class="' . $tab_active_page1 . ' sec-file-tab nav-tab">Tab 1</a>
+                <a href="' . get_admin_url() . 'admin.php?page=nth-instructions&tab=2" id="tab-page2" class="' . $tab_active_page2 . ' sec-file-tab nav-tab">Tab 2</a>
+                <a href="' . get_admin_url() . 'admin.php?page=nth-instructions&tab=3" id="tab-page3" class="' . $tab_active_page3 . ' sec-file-tab nav-tab">Tab 3</a>
+                <a href="' . get_admin_url() . 'admin.php?page=nth-instructions&tab=4" id="tab-page4" class="' . $tab_active_page4 . ' sec-file-tab nav-tab">Tab 4</a>
+            </h2>
+        ';
+
+        echo "
+            <div class='tab-section' style='display: $show_tab_page1' id='page1'>
+                <h2>Page 1</h2>
+                <p>Content pending</p>
+            </div>
+        ";
+        echo "
+            <div class='tab-section' style='display: $show_tab_page2' id='page1'>
+                <h2>Page 2</h2>
+                <p>Content pending</p>
+            </div>
+        ";
+        echo "
+            <div class='tab-section' style='display: $show_tab_page3' id='page1'>
+                <h2>Page 3</h2>
+                <p>Content pending</p>
+            </div>
+        ";
+        echo "
+            <div class='tab-section' style='display: $show_tab_page4' id='page1'>
+                <h2>Page 4</h2>
+                <p>Content pending</p>
+            </div>
+        ";
     }
 }
 
