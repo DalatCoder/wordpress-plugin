f9a9787c62340c6f4fe3f4cd03a34f1b1bed73a6
DalatCoder <hieuntctk42@gmail.com>
2021-07-16 11:14:24 +0700

    Quicktags Creator :zap:

--- 01.basic/wordpress/wp-content/plugins/13.quicktags-creator/index.php
+++ 01.basic/wordpress/wp-content/plugins/13.quicktags-creator/index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /*
@@ -10,3 +9,48 @@ Author: tronghieu
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+
+class NTHQuicktagsCreator
+{
+    function __construct()
+    {
+        add_action('admin_print_footer_scripts', [$this, 'addQuickTag']);
+    }
+
+    function addQuicktag()
+    {
+        if (wp_script_is('quicktags')) {
+?>
+
+            <script type="text/javascript">
+                // This function is used to retrieve the selected text from the text editor
+                function getSel() {
+                    var textArea = document.getElementById('content');
+                    var start = textArea.selectionStart;
+                    var finish = textArea.selectionEnd;
+                    return textArea.value.substring(start, finish);
+                }
+
+                QTags.addButton(
+                    "date_quicktag", "Today's Date", get_date
+                );
+                QTags.addButton(
+                    "myname_quicktag", "Hello Trong Hieu", get_myname
+                );
+
+                function get_date() {
+                    var selected_text = getSel();
+                    QTags.insertContent(Date('d-m-Y'));
+                }
+
+                function get_myname() {
+                    var selected_text = getSel();
+                    QTags.insertContent('Chào Trọng Hiếu nhá');
+                }
+            </script>
+
+<?php }
+    }
+}
+
+$instance = new NTHQuicktagsCreator();
