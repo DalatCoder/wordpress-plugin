f7ab7c5bcf34766232d0ba8450baa3930fe13080
DalatCoder <hieuntctk42@gmail.com>
2021-07-19 21:17:32 +0700

    Modular Admin SubPages :zap:

--- wordpress-plugin-development/wordpress/wp-content/plugins/nth-plugin/inc/Pages/Admin.php
+++ wordpress-plugin-development/wordpress/wp-content/plugins/nth-plugin/inc/Pages/Admin.php
@@ -12,27 +12,67 @@ use \Inc\Api\SettingsApi;
 class Admin extends BaseController
 {
     public $settings;
+    public $pages = array();
+    public $subpages = array();
+
     public function __construct()
     {
         $this->settings = new SettingsApi();
-    }
 
-    public function register()
-    {
-        $pages = [
+        $this->pages = [
             [
-                'page_title' => 'NTN Plugin',
-                'menu_title' => 'NTN Plugin',
+                'page_title' => 'NTH Plugin',
+                'menu_title' => 'NTH Plugin',
                 'capability' => 'manage_options',
                 'menu_slug' => 'nth_plugin',
                 'callback' => function () {
-                    echo '';
+                    echo 'NTH Plugin';
                 },
                 'icon_url' => 'dashicons-store',
                 'position' => 110
             ]
         ];
 
-        $this->settings->addPages($pages)->register();
+        $this->subpages = [
+            [
+                'parent_slug' => 'nth_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'nth_cpt',
+                'callback' => function () {
+                    echo 'Custom Post Types Manager';
+                },
+            ],
+            [
+                'parent_slug' => 'nth_plugin',
+                'page_title' => 'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
+                'capability' => 'manage_options',
+                'menu_slug' => 'nth_taxonomies',
+                'callback' => function () {
+                    echo 'Taxonomies Manager';
+                },
+            ],
+            [
+                'parent_slug' => 'nth_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'nth_widgets',
+                'callback' => function () {
+                    echo 'Widgets Manager';
+                },
+            ],
+        ];
+    }
+
+    public function register()
+    {
+        $this->settings
+            ->addPages($this->pages)
+            ->withSubPage('Dashboard')
+            ->addSubpages($this->subpages)
+            ->register();
     }
 }
